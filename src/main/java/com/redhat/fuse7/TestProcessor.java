cd80acde58b059a61d40a993209318c592107bc7
Chandra Shekhar Pandey <cpandey@cpandey.pnq.csb>
2020-09-18 00:05:19 +0530

    fuse poc

---
+++ src/main/java/com/redhat/fuse7/TestProcessor.java
@@ -0,0 +1,14 @@
+package com.redhat.fuse7;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class TestProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
